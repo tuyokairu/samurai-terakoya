73d771de36a33e4499c56ec5e32d48bb97d0b0d3
tuyokairu <yuri492@yahoo.co.jp>
2025-05-02 23:38:44 +0900

    Delete Q＆A/main directory

--- Q＆A/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.nagoyameshi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.nagoyameshi.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
